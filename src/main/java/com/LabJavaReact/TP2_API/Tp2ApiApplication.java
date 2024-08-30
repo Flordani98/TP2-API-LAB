678e740adbd8ab69b9f71d6f2b62d08b0a4036ae
Florencia <flordani98@gmail.com>
2024-08-30 11:13:31 -0300

    initial commit

---
+++ src/main/java/com/LabJavaReact/TP2_API/Tp2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.LabJavaReact.TP2_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LabJavaReact/TP2_API/Tp2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LabJavaReact.TP2_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
