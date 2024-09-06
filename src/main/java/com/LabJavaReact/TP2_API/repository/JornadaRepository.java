2da350ca964ebb6eea3f7fb246709e7beef768a5
Florencia <flordani98@gmail.com>
2024-09-06 18:49:41 -0300

    implementa validación hsTrabajadas de Empleado en Jornada(reglas de negocio 1-2-3-4)

--- src/main/java/com/LabJavaReact/TP2_API/repository/JornadaRepository.java
+++ src/main/java/com/LabJavaReact/TP2_API/repository/JornadaRepository.java
@@ -1,7 +1,16 @@
 package com.LabJavaReact.TP2_API.repository;
 
+import com.LabJavaReact.TP2_API.model.Empleado;
 import com.LabJavaReact.TP2_API.model.Jornada;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface JornadaRepository extends JpaRepository<Jornada, Long> {
+
+    List<Jornada> findAllByEmpleadoAndFecha(Empleado empleado, LocalDate fecha);
+    List<Jornada> findAllByEmpleado(Empleado empleado);
+
+
 }
--- src/main/java/com/LabJavaReact/TP2_API/service/impl/JornadaService.java
+++ src/main/java/com/LabJavaReact/TP2_API/service/impl/JornadaService.java
@@ -3,7 +3,6 @@ package com.LabJavaReact.TP2_API.service.impl;
 import com.LabJavaReact.TP2_API.dto.JornadaCreateDTO;
 import com.LabJavaReact.TP2_API.dto.JornadaViewDTO;
 import com.LabJavaReact.TP2_API.exception.BadCustomerRequestException;
-import com.LabJavaReact.TP2_API.exception.ResourceNotFoundException;
 import com.LabJavaReact.TP2_API.mapper.JornadaCreateDTOMapper;
 import com.LabJavaReact.TP2_API.mapper.JornadaViewDTOMapper;
 import com.LabJavaReact.TP2_API.model.Concepto;
@@ -15,9 +14,12 @@ import com.LabJavaReact.TP2_API.repository.JornadaRepository;
 import com.LabJavaReact.TP2_API.service.IJornadaService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.temporal.WeekFields;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 @Service
 public class JornadaService implements IJornadaService {
@@ -53,12 +55,98 @@ public class JornadaService implements IJornadaService {
         Concepto concepto = conceptoRepository.findById(jornadaCreateDTO.getIdConcepto())
                 .orElseThrow(() -> new BadCustomerRequestException("No existe el concepto ingresado"));
 
-        validarHsTrabajadasSegunConcepto(concepto.getNombre(), jornadaCreateDTO.getHsTrabajadas());
+        validarHsTrabajadas(concepto, jornadaCreateDTO.getHsTrabajadas());
+        validarHsTrabajadasDiaEspecifico(empleado, jornadaCreateDTO.getFecha(), jornadaCreateDTO.getHsTrabajadas());
+        validarHsTrabajadasSemanal(empleado, jornadaCreateDTO.getHsTrabajadas(), jornadaCreateDTO.getFecha());
+        validarHsTrabajadasMensual(empleado, jornadaCreateDTO.getHsTrabajadas(), jornadaCreateDTO.getFecha());
 
         Jornada jornada = jornadaRepository.save(JornadaCreateDTOMapper.toEntity(jornadaCreateDTO, empleado, concepto));
 
         return JornadaViewDTOMapper.toDTO(jornada);
     }
+
+
+    private void validarHsTrabajadas(Concepto concepto, Integer hsTrabajadas){
+        validarHsTrabajadasSegunConcepto(concepto.getNombre(), hsTrabajadas);
+        validarhsTrabajadasDentroRangoHorario(concepto.getHsMinimo(), concepto.getHsMaximo(), hsTrabajadas);
+    }
+
+    private void validarHsTrabajadasDiaEspecifico(Empleado empleado, LocalDate fecha,  Integer hsTrabajadasSolicitadas){
+        List<Jornada> jornadasDiaEspecifico = jornadaRepository.findAllByEmpleadoAndFecha(empleado, fecha);
+        int horasTotalesEmpleado = 0;
+
+        for (Jornada jornada: jornadasDiaEspecifico) {
+            horasTotalesEmpleado = horasTotalesEmpleado + jornada.getHsTrabajadas();
+        }
+        if(horasTotalesEmpleado + hsTrabajadasSolicitadas > 14){
+            throw new BadCustomerRequestException("Un empleado no puede cargar más de 14 horas trabajadas en un día.");
+        }
+
+    }
+    private void validarHsTrabajadasSemanal(Empleado empleado, Integer hsTrabajadasSolicitadas, LocalDate fechaSolicitada){
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+        int semana = fechaSolicitada.get(weekFields.weekOfYear());
+        int anio = fechaSolicitada.getYear();
+
+        List<Jornada> jornadasEmpleado = jornadaRepository.findAllByEmpleado(empleado);
+        List<Jornada> jornadasSemanaEspecifica = filtrarJornadasSegunSemanayAnio(jornadasEmpleado, anio, semana);
+
+        int hsTrabajadasSemanal = sumarHsTrabajadasListaJornadas(jornadasSemanaEspecifica);
+
+        //sumar las hsTrabajadas de la semana de la fecha solicitada + hsTrabajadasSolicitadas
+        if(hsTrabajadasSemanal + hsTrabajadasSolicitadas > 52){
+            throw new BadCustomerRequestException("El empleado ingresado supera las 52 horas semanales.");
+        }
+
+    }
+    private void validarHsTrabajadasMensual(Empleado empleado, Integer hsTrabajadasSolicitadas, LocalDate fechaSolicitada){
+        int mes = fechaSolicitada.getMonthValue();
+        int anio = fechaSolicitada.getYear();
+
+        List<Jornada> jornadasEmpleado = jornadaRepository.findAllByEmpleado(empleado);
+        List<Jornada> jornadasMesEspecifico = filtrarJornadasSegunMesyAnio(jornadasEmpleado, anio, mes);
+
+        int hsTrabajadasMensual = sumarHsTrabajadasListaJornadas(jornadasMesEspecifico);
+
+        //sumar las hsTrabajadas de la semana de la fecha solicitada + hsTrabajadasSolicitadas
+        if(hsTrabajadasMensual + hsTrabajadasSolicitadas > 190){
+            throw new BadCustomerRequestException("El empleado ingresado supera las 190 horas mensuales.");
+        }
+
+    }
+
+    private int sumarHsTrabajadasListaJornadas(List<Jornada> jornadas){
+        int hsTotales = 0;
+        for (Jornada jornada : jornadas) {
+            hsTotales = hsTotales + jornada.getHsTrabajadas();
+        }
+        return hsTotales;
+    }
+
+    private List<Jornada> filtrarJornadasSegunAnio(List<Jornada> jornadas, int anio){
+        return jornadas.stream().filter(jornada -> jornada.getFecha().getYear() == anio).collect(Collectors.toList());
+    }
+
+    private List<Jornada> filtrarJornadasSegunSemanayAnio(List<Jornada> jornadas, int anio, int semana){
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+        List<Jornada> jornadasMismoAnio = filtrarJornadasSegunAnio(jornadas, anio);
+
+        return jornadasMismoAnio.stream().filter(jornada -> jornada.getFecha().get(weekFields.weekOfYear()) == semana).collect(Collectors.toList());
+
+    }
+    private List<Jornada> filtrarJornadasSegunMesyAnio(List<Jornada> jornadas, int anio, int mes){
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+        List<Jornada> jornadasMismoAnio = filtrarJornadasSegunAnio(jornadas, anio);
+
+
+        return jornadasMismoAnio.stream().filter(jornada -> jornada.getFecha().getMonthValue() == mes).collect(Collectors.toList());
+
+    }
+//    private List<Jornada> filtrarJornadasSegunSemana(List<Jornada> jornadasMismoAnio, int semana){
+//        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+//        return jornadasMismoAnio.stream().filter(jornada -> jornada.getFecha().get(weekFields.weekOfYear()) == semana).collect(Collectors.toList());
+//
+//    }
 
     private void validarHsTrabajadasSegunConcepto(String concepto, Integer hsTrabajadas){
         boolean esConceptoLaborable = !concepto.equals("Día Libre");
@@ -69,6 +157,18 @@ public class JornadaService implements IJornadaService {
             throw new BadCustomerRequestException("El concepto ingresado no requiere el ingreso de 'hsTrabajadas'");
 
         }
+    }
+
+    private void validarhsTrabajadasDentroRangoHorario(int hsMinimo, int hsMaximo, Integer hsTrabajadas){
+//        boolean estaDentroRango = hsMinimo <= hsTrabajadas && hsMaximo <= hsTrabajadas;
+        boolean estaDentroRango = hsTrabajadas >= hsMinimo && hsTrabajadas <= hsMaximo;
+
+        if(!estaDentroRango){
+            throw new BadCustomerRequestException("El rango de horas que se puede cargar para este concepto es de "
+            + hsMinimo + " - " + hsMaximo);
+        }
+
+
     }
 
 
